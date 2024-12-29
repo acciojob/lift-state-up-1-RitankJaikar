6136136e8f91481f4e3b95c428190d5c6293b693
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-29 12:59:19 +0530

    Submit

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import React from "react";
 const Child = ({ onShowModal }) => {
   return (
     <div>
-      <h3>Child Component</h3>
+      <h3>Modal Content</h3>
       <button onClick={onShowModal}>Show Modal</button>
     </div>
   );
